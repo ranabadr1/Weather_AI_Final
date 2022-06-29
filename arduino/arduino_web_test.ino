d3aa307d9bee9945bbc0375b24df1ec4cb38da4a
ranabadr1 <108117929+ranabadr1@users.noreply.github.com>
2022-06-29 13:07:04 +0300

    Add files via upload

---
+++ arduino/arduino_web_test.ino
@@ -0,0 +1,84 @@
+#include <ESP8266WiFi.h>
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+/************************* WiFi Access Point *********************************/
+#define WLAN_SSID       "samsung"
+#define WLAN_PASS       "12345678"
+/************************* Adafruit.io Setup *********************************/
+#define AIO_SERVER      "io.adafruit.com"
+#define AIO_SERVERPORT  1883                   // use 8883 for SSL
+#define AIO_USERNAME    "tonialakik"
+#define AIO_KEY         "aio_wQkR20kT1g9QcXm1rHGTFa3MHKoZ"
+/************* DHT11 Setup ********************************************/
+int outputpin= A0;
+/************ Global State (you don't need to change this!) ******************/
+// Create an ESP8266 WiFiClient class to connect to the MQTT server.
+WiFiClient client;
+// or... use WiFiFlientSecure for SSL
+//WiFiClientSecure client;
+// Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
+Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
+/****************************** Feeds ***************************************/
+// Setup a feed called 'photocell' for publishing.
+// Notice MQTT paths for AIO follow the form: <username>/feeds/<feedname>
+Adafruit_MQTT_Publish temperature = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/Temp");
+/*************************** Sketch Code ************************************/
+// Bug workaround for Arduino 1.6.6, it seems to need a function declaration
+// for some reason (only affects ESP8266, likely an arduino-builder bug).
+void MQTT_connect();
+void setup() {
+  Serial.begin(9600);
+  Serial.println(F("Adafruit MQTT demo"));
+  // Connect to WiFi access point.
+  Serial.println(); Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(WLAN_SSID);
+  WiFi.begin(WLAN_SSID, WLAN_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+  Serial.println("WiFi connected");
+  Serial.println("IP address: "); Serial.println(WiFi.localIP());
+}
+uint32_t x=0;
+void loop() {
+  // Ensure the connection to the MQTT server is alive (this will make the first
+  // connection and automatically reconnect when disconnected).  See the MQTT_connect
+  // function definition further below.
+  MQTT_connect();
+  // Read humidity
+int analogValue = analogRead(outputpin);
+float millivolts = (analogValue/1200.0) * 3300; //3300 is the voltage provided by NodeMCU
+float celsius = millivolts/10;
+Serial.print("in DegreeC=   ");
+Serial.println(celsius);
+  Serial.print(F("\nTemperature: "));
+  Serial.print(celsius);
+  temperature.publish(celsius);
+  delay(60000);
+}
+// Function to connect and reconnect as necessary to the MQTT server.
+// Should be called in the loop function and it will take care if connecting.
+void MQTT_connect() {
+  int8_t ret;
+  // Stop if already connected.
+  if (mqtt.connected()) {
+    return;
+  }
+  Serial.print("Connecting to MQTT... ");
+  uint8_t retries = 3;
+  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+       Serial.println(mqtt.connectErrorString(ret));
+       Serial.println("Retrying MQTT connection in 5 seconds...");
+       mqtt.disconnect();
+       delay(5000);  // wait 5 seconds
+       retries--;
+       if (retries == 0) {
+         // basically die and wait for WDT to reset me
+         while (1);
+       }
+  }
+  Serial.println("MQTT Connected!");
+}
